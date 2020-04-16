a78f2e0764a938e1e0434bcf85200dd755f38d14
soulx <170918810@qq.com>
2020-04-16 19:29:32 +0800

    AQS学习

--- src/main/java/condition/Demo.java
+++ src/main/java/condition/Demo.java
@@ -1,23 +1,31 @@
 package condition;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.*;
 
 public class Demo {
 	
 	private final Lock lock = new ReentrantLock();
+	private final ReadWriteLock lock2 = new ReentrantReadWriteLock();
 	private final Condition condition = lock.newCondition();
-	
+	private CountDownLatch countDownLatch = new CountDownLatch(1);
+	private CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
 	private class T implements Runnable {
 		@Override
 		public void run() {
 			try {
 				lock.lock();
+//				lock2.lockInterruptibly();
 				try {
 					System.out.println("开始等待");
 					condition.await();
-					System.out.println(Thread.currentThread().isInterrupted());
+					condition.awaitNanos(1000);
+					System.out.println("唤醒");
+//					while (true){
+//						Thread.sleep(1);
+//					}
+//					System.out.println(Thread.currentThread().isInterrupted());
 				} finally {
 					lock.unlock();
 				}
@@ -28,21 +36,34 @@ public class Demo {
 		
 	}
 	
-	public void signal() {
+	public void signal() throws InterruptedException {
 		lock.lock();
 		try {
 			System.out.println("获得锁");
 			condition.signalAll();
+			condition.signal();
+			while (true){
+						Thread.sleep(1);
+					}
+//			System.out.println("未解锁");
 		} finally {
 			lock.unlock();
 		}
 	}
-	
+
+	public void testCondition(){
+		try {
+			condition.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) throws InterruptedException {
 		Demo demo = new Demo();
 		new Thread(demo.new T()).start();
 		Thread.sleep(2000);
 		demo.signal();
+//		demo.testCondition();
 	}
 
 }
